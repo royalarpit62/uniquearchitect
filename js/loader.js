992f2ff08af426a232da1d8a0b2e1feb3d7e05de
royalarpit62 <44779301+royalarpit62@users.noreply.github.com>
2020-09-16 22:05:16 +0530

    Add files via upload

---
+++ js/loader.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.modal').modal();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('.slider').slider({
+        full_width: true
+    });
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.myreviews').carousel({
+        numVisible: 7,
+        shift: 55,
+        padding: 55
+    });
+
+})
